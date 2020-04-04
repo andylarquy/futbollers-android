6925295b522e0f575b2a3e0bb015dad658441e83
Andres Suarez <andres27059934@gmail.com>
2020-04-04 18:07:26 -0300

    Maquetado login

--- app/src/main/java/ar/edu/unsam/proyecto/futbollers/MainActivity.kt
+++ app/src/main/java/ar/edu/unsam/proyecto/futbollers/MainActivity.kt
@@ -2,11 +2,17 @@ package ar.edu.unsam.proyecto.futbollers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_login.setOnClickListener({
+            Toast.makeText(this@MainActivity, "Hiciste Click! :)", Toast.LENGTH_SHORT).show()
+        })
     }
 }
