e8d25bd639c8eb323bc1a17447e74b02f0431142
Andres Suarez <andres27059934@gmail.com>
2020-06-16 21:38:41 -0300

    Continuado maquetado de crear equipo

--- app/src/main/java/ar/edu/unsam/proyecto/futbollers/activities/nuevoEquipo/NuevoEquipoActivity.kt
+++ app/src/main/java/ar/edu/unsam/proyecto/futbollers/activities/nuevoEquipo/NuevoEquipoActivity.kt
@@ -1,28 +1,70 @@
 package ar.edu.unsam.proyecto.futbollers.activities.nuevoEquipo
 
+import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import ar.edu.unsam.proyecto.futbollers.R
+import ar.edu.unsam.proyecto.futbollers.activities.armarPartido.steps.ElegirCanchaFragment
+import ar.edu.unsam.proyecto.futbollers.domain.Cancha
+import ar.edu.unsam.proyecto.futbollers.domain.Usuario
+import ar.edu.unsam.proyecto.futbollers.services.UsuarioLogueado
+import ar.edu.unsam.proyecto.futbollers.services.UsuarioService
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.callbacks.onDismiss
+import com.afollestad.materialdialogs.list.customListAdapter
 import com.bumptech.glide.Glide
 import com.esafirm.imagepicker.features.ImagePicker
 import com.esafirm.imagepicker.features.ReturnMode
 import com.esafirm.imagepicker.model.Image
+import com.leodroidcoder.genericadapter.BaseRecyclerListener
+import com.leodroidcoder.genericadapter.BaseViewHolder
+import com.leodroidcoder.genericadapter.GenericRecyclerViewAdapter
+import com.leodroidcoder.genericadapter.OnRecyclerItemClickListener
+import com.squareup.picasso.Picasso
 import es.dmoral.toasty.Toasty
 import kotlinx.android.synthetic.main.activity_nuevo_equipo.*
+import kotlinx.android.synthetic.main.row_elegir_jugador_para_equipo.view.*
 
-class NuevoEquipoActivity : AppCompatActivity() {
+val usuarioService = UsuarioService
+val usuarioLogueado = UsuarioLogueado.usuario
+
+class NuevoEquipoActivity : AppCompatActivity(), IntegranteListener {
 
     var nombreSeleccionado: String = ""
+
+    lateinit var integrantesAdapter: IntegrantesDeEquipoAdapter
+    lateinit var rv: RecyclerView
+    lateinit var dialogAgregarAmigo: MaterialDialog
+    val integrantes: MutableList<Usuario> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nuevo_equipo)
+
+        rv = integrantes_de_equipo_list
+        rv.setHasFixedSize(true)
+
+        val llm = LinearLayoutManager(this)
+        rv.layoutManager = llm
+
+        integrantesAdapter = IntegrantesDeEquipoAdapter(this, this)
+
+        rv.adapter = integrantesAdapter
+        usuarioService.getAmigosDelUsuario(this, usuarioLogueado, ::callbackAmigosDelUsuario)
+
+        setupDialogAgregarAmigo()
 
         input_nombre_equipo.addTextChangedListener(object : TextWatcher {
             //Estos metodos tienen que estar implementados, burocracia
@@ -43,9 +85,30 @@ class NuevoEquipoActivity : AppCompatActivity() {
             uploadImage()
         }
 
+        btn_agregar_integrante.setOnClickListener{
 
+        }
 
+    }
 
+    fun setupDialogAgregarAmigo() {
+        //Preparo el dialog de amigos
+        dialogAgregarAmigo = MaterialDialog(this)
+            .title(text = "Agrega un integrante")
+            //TODO: Implementar adapter de integrantes
+            //.customListAdapter(agregarIntegranteAdapter)
+            .noAutoDismiss()
+            .negativeButton(text = "Cerrar") {
+                it.dismiss()
+            }
+
+    }
+
+    fun callbackAmigosDelUsuario(amigos: MutableList<Usuario>){
+        //FIXME: Crear una variable temporal, los integrantes no venian del back
+        integrantesAdapter.clear()
+        integrantesAdapter.addAll(amigos)
+        integrantesAdapter.notifyDataSetChanged()
     }
 
     fun uploadImage(){
@@ -75,6 +138,51 @@ class NuevoEquipoActivity : AppCompatActivity() {
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
+
+    override fun onDeleteClick(position: Int) {
+        val integrante = integrantesAdapter.getItem(position)
+        integrantes.remove(integrante)
+        integrantesAdapter.notifyDataSetChanged()
+    }
+
+
+}
+
+class IntegrantesDeEquipoAdapter(context: Context, listener: NuevoEquipoActivity) :
+    GenericRecyclerViewAdapter<Usuario, IntegranteListener, IntegrantesDeEquipoViewHolder>(
+        context,
+        listener
+    ) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): IntegrantesDeEquipoViewHolder {
+        return IntegrantesDeEquipoViewHolder(inflate(R.layout.row_elegir_jugador_para_equipo, parent), listener)
+    }
+}
+
+class IntegrantesDeEquipoViewHolder(itemView: View, listener: IntegranteListener?) :
+    BaseViewHolder<Usuario, IntegranteListener>(itemView, listener) {
+
+    private val amigoFoto: ImageView = itemView.amigo_foto
+    private val amigoNombre: TextView = itemView.amigo_nombre
+    private val posicionAmigo: TextView = itemView.posicion_amigo
+
+    init {
+        listener?.run {
+            itemView.trash_icon.setOnClickListener { onDeleteClick(adapterPosition) }
+        }
+    }
+
+    override fun onBind(item: Usuario) {
+        amigoNombre.text = item.nombre
+        posicionAmigo.text = item.posicion
+        Picasso.get().load(item.foto).into(amigoFoto)
+    }
+
+}
+
+interface IntegranteListener : BaseRecyclerListener {
+
+    fun onDeleteClick(position: Int)
 
 }
 
