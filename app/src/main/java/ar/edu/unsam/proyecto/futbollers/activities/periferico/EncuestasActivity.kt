ac8ed5aba189ff222ef71ad32660fd0904be5a76
Andres Suarez <andres27059934@gmail.com>
2020-07-02 18:45:02 -0300

    Iniciado bindeo de radio buttons

--- app/src/main/java/ar/edu/unsam/proyecto/futbollers/activities/periferico/EncuestasActivity.kt
+++ app/src/main/java/ar/edu/unsam/proyecto/futbollers/activities/periferico/EncuestasActivity.kt
@@ -1,33 +1,29 @@
 package ar.edu.unsam.proyecto.futbollers.activities.periferico
 
 import android.content.Context
-import android.graphics.Bitmap
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.RadioButton
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import ar.edu.unsam.proyecto.futbollers.R
 import ar.edu.unsam.proyecto.futbollers.domain.Encuesta
-import ar.edu.unsam.proyecto.futbollers.domain.Usuario
 import ar.edu.unsam.proyecto.futbollers.services.UsuarioLogueado
 import ar.edu.unsam.proyecto.futbollers.services.auxiliar.EncuestaService
 import com.afollestad.materialdialogs.MaterialDialog
 import com.afollestad.materialdialogs.callbacks.onDismiss
 import com.afollestad.materialdialogs.customview.customView
-import com.afollestad.materialdialogs.list.customListAdapter
-import com.bumptech.glide.Glide
+import com.afollestad.materialdialogs.customview.getCustomView
 import com.leodroidcoder.genericadapter.BaseRecyclerListener
 import com.leodroidcoder.genericadapter.BaseViewHolder
 import com.leodroidcoder.genericadapter.GenericRecyclerViewAdapter
 import com.squareup.picasso.Picasso
+import es.dmoral.toasty.Toasty
 import kotlinx.android.synthetic.main.activity_encuesta.*
+import kotlinx.android.synthetic.main.dialog_formulario_encuesta.view.*
 import kotlinx.android.synthetic.main.row_encuesta.view.*
 
 
@@ -39,6 +35,11 @@ class EncuestasActivity : AppCompatActivity(), ContestarEncuestaClickListener {
 
     val encuestaService = EncuestaService
     val usuarioLogueado = UsuarioLogueado.usuario
+    val encuestaSeleccionada: Encuesta? = EncuestasContext.encuestaSeleccionada
+
+    var respuesta1: Boolean = false
+    var respuesta2: Boolean = false
+    var respuesta3: Boolean = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -56,13 +57,20 @@ class EncuestasActivity : AppCompatActivity(), ContestarEncuestaClickListener {
         rv.adapter = encuestasAdapter
 
         encuestaService.getEncuestasDelUsuario(this, usuarioLogueado, ::callbackEncuesta)
-
     }
 
     fun callbackEncuesta(encuestas: MutableList<Encuesta>){
         encuestasAdapter.clear()
         encuestasAdapter.items = encuestas
         encuestasAdapter.notifyDataSetChanged()
+    }
+
+    fun callbackUpdateEncuesta(){
+        Toasty.success(this, "Estoy impactada!!",Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onContestarEncuestaClick(position: Int) {
+        dialogContestarEncuesta.show()
     }
 
     fun setupDialogContestarEncuesta() {
@@ -72,19 +80,50 @@ class EncuestasActivity : AppCompatActivity(), ContestarEncuestaClickListener {
             .customView(R.layout.dialog_formulario_encuesta)
             .noAutoDismiss()
             .positiveButton(text = "Aceptar") {
+
+                encuestaSeleccionada?.respuesta1 = respuesta1
+                encuestaSeleccionada?.respuesta2 = respuesta2
+                encuestaSeleccionada?.respuesta3 = respuesta3
+
+                encuestaService.updateEncuesta(this, encuestaSeleccionada!!, ::callbackUpdateEncuesta)
+                //TODO: Toast de que todo piola
+
                 it.dismiss()
             }
             .negativeButton(text = "Cancelar") {
+                respuesta1 = false
+                respuesta2 = false
+                respuesta3 = true
+
+                Toasty.error(this, "You only had to follow the damn train", Toast.LENGTH_SHORT).show()
+
                 it.dismiss()
             }
             .onDismiss{
                 //TODO: Algo?
             }
 
-    }
+        val customView = dialogContestarEncuesta.getCustomView()
 
-    override fun onContestarEncuestaClick(position: Int) {
-        dialogContestarEncuesta.show()
+
+        val radioGroup = customView.first
+        radioGroup.setOnCheckedChangeListener { _, checkedId ->
+
+            when (checkedId) {
+
+                2131231233 -> {
+                    respuesta1 = true
+                }
+
+                2131231230 -> {
+                    respuesta1 = false
+                }
+            }
+
+            Log.i("EncuestasActivity", respuesta1.toString())
+        }
+
+
     }
 
 }
@@ -118,14 +157,18 @@ class EncuestaViewHolder(itemView: View, listener: ContestarEncuestaClickListene
     }
 
     override fun onBind(item: Encuesta) {
-       //TODO Bindear sarasa
         jugadoNombre.text = item.usuarioReferenciado!!.nombre
         Picasso.get().load(item.usuarioReferenciado.foto).into(jugadorFoto)
+        EncuestasContext.encuestaSeleccionada = item
     }
 
 }
 
 interface ContestarEncuestaClickListener : BaseRecyclerListener {
     fun onContestarEncuestaClick(position: Int)
+}
+
+object EncuestasContext{
+    var encuestaSeleccionada: Encuesta? = null
 }
 
