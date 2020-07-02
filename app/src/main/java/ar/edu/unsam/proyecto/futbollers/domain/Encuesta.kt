f6703945fc67dc05a27d5fbcea9b7096a46c5d07
Andres Suarez <andres27059934@gmail.com>
2020-07-02 18:45:02 -0300

    Iniciado bindeo de radio buttons

--- app/src/main/java/ar/edu/unsam/proyecto/futbollers/domain/Encuesta.kt
+++ app/src/main/java/ar/edu/unsam/proyecto/futbollers/domain/Encuesta.kt
@@ -1,13 +1,23 @@
 package ar.edu.unsam.proyecto.futbollers.domain
 
+import android.util.Log
+import com.google.gson.Gson
+import org.json.JSONObject
+
 class Encuesta {
 
     val idEncuesta: Long? = null
     val usuarioEncuestado: Usuario? = null
     val usuarioReferenciado: Usuario? = null
     val partido: Partido? = null
-    val respuesta1: Boolean? = null
-    val respuesta2: Boolean? = null
-    val respuesta3: Boolean? = null
+    var respuesta1: Boolean? = null
+    var respuesta2: Boolean? = null
+    var respuesta3: Boolean? = null
+
+    fun toJson(o: Encuesta): JSONObject?{
+        val jsonResult = JSONObject(Gson().toJson(o))
+        Log.i("EncuestasActivity", "Encuesta parseada a Json: $jsonResult")
+        return jsonResult
+    }
 
 }
--- app/src/main/java/ar/edu/unsam/proyecto/futbollers/services/auxiliar/EncuestaService.kt
+++ app/src/main/java/ar/edu/unsam/proyecto/futbollers/services/auxiliar/EncuestaService.kt
@@ -9,9 +9,11 @@ import ar.edu.unsam.proyecto.futbollers.services.EmpresaService
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.google.gson.Gson
 import org.json.JSONArray
+import org.json.JSONObject
 
 object EncuestaService {
 
@@ -40,6 +42,30 @@ object EncuestaService {
 
         queue.add(request)
 
+    }
+
+    fun updateEncuesta(context: Context, encuesta: Encuesta, callback: () -> Unit){
+        val queue = Volley.newRequestQueue(context)
+
+        val url = "${Constants.BASE_URL}/encuestas"
+
+        val request = JsonObjectRequest(
+            Request.Method.PUT, url, Encuesta().toJson(encuesta),
+
+            Response.Listener<JSONObject> { response ->
+
+                Log.i("EncuestasActivity", "[DEBUG]:Communication with API Rest Suceeded")
+
+                Log.i("EncuestasActivity", response.toString())
+                callback()
+            },
+            Response.ErrorListener {
+                Log.i("EncuestasActivity", "[DEBUG]:Communication with API Rest Failed")
+                handleError(context, it, EmpresaService::lambdaManejoErrores)
+            })
+        request.retryPolicy = Constants.defaultPolicy
+
+        queue.add(request)
     }
 
 }
